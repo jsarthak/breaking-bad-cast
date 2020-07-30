16abdf41bc0c61d6f958415cf1187a04311235ec
jsarthak <jsarthak@outlook.com>
2020-07-30 09:02:17 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, {useState, useEffect} from 'react';
+import './App.css';
+import axios from 'axios';
+import Header from './components/ui/header'
+import CharacterGrid from './components/characters/characterGrid'
+import Search from './components/ui/search';
+const App = () => {
+
+  const [items, setItems]= useState([])
+  const [isLoading, setIsLoading]= useState(true)
+  const [query, setQuery] = useState('')
+
+  useEffect(()=>{
+    const fetchItems = async()=>{
+      const result = await axios(`https://www.breakingbadapi.com/api/characters?name=${query}`)
+      setItems(result.data)
+      setIsLoading(false)
+    }
+    fetchItems()
+  },[query])
+
+  return (
+    <div className="container">
+      <Header/>
+      <Search getQuery={(q)=>{setQuery(q)}}/>
+      <CharacterGrid isLoading={isLoading} items={items}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/characters/characterGrid.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import CharacterItem from './characterItem'
+import Spinner from '../ui/spinner'
+
+const CharacterGrid = ({ items, isLoading }) => {
+    return isLoading ? <Spinner/> : (
+        <section className="cards">
+            {items.map((item) => (
+                <CharacterItem key={item.char_id} item={item} ></CharacterItem>
+            ))}
+        </section>
+    )
+}
+
+export default CharacterGrid
